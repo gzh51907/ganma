3fa6644ab4311ab66e5a39fdc95df933e328cb88
lkr997 <52983575+lkr997@users.noreply.github.com>
2019-10-10 18:57:04 +0800

    1

--- ganmademo/src/pages/Home.vue
+++ ganmademo/src/pages/Home.vue
@@ -4,3 +4,29 @@
     </div>
 </template>
 
+<script>
+export default {
+    async created() {
+    // 发起ajax请求
+    let sessionId = ''
+    let recId = ''
+    let data = await this.$axios.post("https://api.wzq998.com/api/brand/detail",
+         {
+        data: data,
+      
+    },
+    {
+        headers: {
+
+            'Authorization': 'Bearer ' ,
+            "Cookie" : 'sessionId=' + sessionId + '; recId=' + recId,
+    
+        }
+    }
+
+    );
+    console.log(data)
+  }
+    
+}
+</script>
