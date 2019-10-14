f9bafcd10bdb40c65e9c55841c017615d460ea5d
luojianfeng <1768977091@qq.com>
2019-10-14 11:59:34 +0800

    列表渲染

--- ganmademo/src/mondb/routers/goods.js
+++ ganmademo/src/mondb/routers/goods.js
@@ -20,29 +20,39 @@ const colName = 'goodsinf'
 
 // 查询所有用户
 
-Router.get('/', async (req, res) => {
- 
-})
 
-Router.route('/:id')
-    // 删除
-    .delete((req, res) => {
 
-    })
-    // 用户信息修改
-    .patch((req, res) => {
+//  Router.route('/:brand_id')
+//     // 删除
+//     .delete((req, res) => {
 
-    })
+//     })
+//     // 用户信息修改
+//     .patch((req, res) => {
 
-    // 查询用户
-    .get((req, res) => {
+//     })
 
-        let id = req.params.brand_id
-        let result = await mongo.find(colName, id)
+//     // 查询用户
+//     .get((req, res) => {
+
+//         // let id = req.params.brand_id
+//         // console.log(req.params.brand_id)
+//         // let result = await mongo.find(colName, id)
+        
+//         // res.send(result)
+//     })
+    Router.get('/select', async (req, res) => {
+        
+        let id = req.query.brand_id
+        console.log(req)
+
+        let result = await mongo.find(colName, {'brand_id':Number(id)})
+        // console.log(result)
+        // let result = await mongo.find(colName)
 
         res.send(result)
-    })
 
+    })
 
 
 
